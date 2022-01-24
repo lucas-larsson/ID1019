1dbe0cecd45d7a3892b3a6a864778b44bc2d4538
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-01-24 18:44:40 +0100

    added simplify() function

--- lab_2/lib/lab2.ex
+++ lab_2/lib/lab2.ex
@@ -20,7 +20,11 @@ defmodule Lab2 do
     e = {:add,
          {:mul, {:num, 2}, {:var, :x}},
           {:num, 4}}
-      deriv(e, :x)
+       d = deriv(e, :x)
+       IO.write("Expression: #{pprint(e)}\n")
+       IO.write("Derivative: #{pprint(d)}\n")
+       IO.write("Simplified: #{pprint(simplify(d))}\n")
+       :ok
   end
 
 def pprint({:num, n}) do "#{n}" end
@@ -28,11 +32,11 @@ def pprint({:num, n}) do "#{n}" end
 def pprint({:var, v}) do "#{v}" end
 
 def pprint({:add, e1, e2}) do
-   "#{pprint(e1)} +  #{pprint(e2)}"
+   "(#{pprint(e1)} +  #{pprint(e2)})"
   end
 
-def pprint(:mul, e1, e2)  do
-  "#{pprint(e1)} + #{pprint(e2)}"
+def pprint({:mul, e1, e2}) do
+  "#{pprint(e1)} * #{pprint(e2)}"
   end
 
 @type literal() :: {:num, number()} | {:var, atom()}
@@ -42,10 +46,37 @@ def pprint(:mul, e1, e2)  do
 | {:mul, expr(), expr()}
 
 
+
+def simplify({:add, e1, e2}) do
+  simplify_add(
+    simplify(e1),
+    simplify(e2)
+    )
+end
+
+def simplify({:mul, e1, e2}) do
+  simplify_mull(
+    simplify(e1),
+    simplify(e2)
+  )
+end
+
+@spec simplify(any) :: any
+def simplify(e) do e end
+
+
+def simplify_add({:num, 0}, e) do e end
+def simplify_add( e, {:num, 0}) do e end
+def simplify_add({:num, y}, {:num,x}) do {:num, x + y } end
+
+def simplify_mull({:num, 0}, _) do {:num, 0 } end
+def simplify_mull(_, {:num, 0}) do {:num, 0 }end
+def simplify_mull({:num, 1}, e ) do e end
+def simplify_mull( e, {:num, 1}) do e end
+def simplify_mull(e1, e2) do  e1 * e2  end
+
   def deriv({:num, _ }, _ ) do {:num, 0} end
-
   def deriv({:var, v}, v) do {:num, 1} end
-
   # anything besides " v " because it is mentioned in the function before
   def deriv({:var, _}, _) do {:num, 0} end
 
