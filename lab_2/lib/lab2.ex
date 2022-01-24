15fee027edc918279f065b84287777b081671139
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-01-24 16:47:09 +0100

    first draft of the deriv() function

--- lab_2/lib/lab2.ex
+++ lab_2/lib/lab2.ex
@@ -15,4 +15,33 @@ defmodule Lab2 do
   def hello do
     :world
   end
+@type literal() :: {:num, number()} | {:var, atom()}
+
+@type expr() :: literal()
+| {:add, expr(), expr()}
+| {:mul, expr(), expr()}
+
+
+  def deriv({:num, _ }, _ ) do {:num, 0} end
+
+  def deriv({:var, v}, v) do {:num, 1} end
+
+  # anything besides " v " because it is mentioned in the function before
+  def deriv({:var, _}, _) do {:num, 0} end
+
+  def deriv({:add, e1, e2}, v) do
+    {:add, deriv(e1, v), deriv(e2, v)}
+
+  end
+
+  def deriv({:mul, e1, e2}, v) do
+    {:add,
+     {:mul, deriv(e1, v), e2 },
+     {:mul, e1, deriv(e2, v)}
+    }
+
+  end
+
+
+
 end
