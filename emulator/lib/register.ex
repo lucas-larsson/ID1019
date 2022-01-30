931b9a7d0475cd52821dc885efcd313f183cc058
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-01-30 14:08:24 +0100

    added registers

---
+++ emulator/lib/register.ex
@@ -0,0 +1,15 @@
+defmodule Register do
+
+  # initiate new registers
+  def new() do
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+  end
+
+  
+  def read( _, 0) do 0 end
+  def read(reg, i) do elem(reg, i) end
+
+  def write(reg, 0, _) do reg end
+  def write(reg, i, val) do put_elem(reg, i, val) end
+
+end
