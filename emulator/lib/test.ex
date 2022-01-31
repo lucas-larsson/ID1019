ba04171bd07e3ae89eddd79d231fb428535725a8
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-01-31 15:33:01 +0100

    modefied test data

--- emulator/lib/test.ex
+++ emulator/lib/test.ex
@@ -8,15 +8,27 @@ defmodule Test do
   end
 
   def demo() do
-    [{:addi, 1, 0, 10},    # $1 <- 10
-     {:addi, 2, 0, 5},     # $2 <- 5
-     {:add, 3, 1, 2},      # $3 <- $1 + $2
-     {:sw, 3, 0, 7},       # mem[0 + 7] <- $3
-     {:lw, 4, 0, 7},       # $4 <- mem[0+7]
-     {:addi, 5, 0, 1},     # $5 <- 1
-     {:sub, 4, 4, 5},      # $4 <- $4 - $5
-     {:out, 4},            # out $4
-     {:bne, 4, 0, -3},     # branch if not equal
+    IO.write('Registers 0 - 6 values are printed in the folwing order:
+
+    ')
+
+    [{:addi, 0, 4, 678},    # $0 <- $4 + 678
+     {:out, 0},             # out $0
+     {:addi, 1, 0, 1},      # $1 <- $0 + 1
+     {:out, 1},             # out $1
+     {:addi, 2, 1, 1},      # $2 <- 1 + 1
+     {:out, 2},             # out $2
+     {:add, 3, 1, 2},       # $3 <- $1 + $2
+     {:out, 3},             # out $3
+     {:sw, 2, 3, 1},        # mem[3 + 1] <- $2
+     {:out, 4},             # out $4
+     {:lw, 4, 1, 3},        # $4 <- mem[3 + 1]
+     {:out, 4},             # out $4
+     {:add, 5, 3, 2},       # $5 <- $3 + $2
+     {:out, 5},             # out $5
+     {:sub, 6, 5, 4},       # $6 <- $5 - $4
+     {:out, 6},             # out $6
+     {:bne, 4, 0, -3},      # branch if not equal
      {:halt}]
   end
 
