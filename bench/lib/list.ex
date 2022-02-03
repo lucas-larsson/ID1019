b067db74bd0e0724b63fb58bd33a2ad721990958
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-03 20:53:42 +0100

    removed file with wrong configration

--- bench/lib/list.ex
+++
@@ -1,9 +0,0 @@
-defmodule List do
-
-  def list_new() do [] end
-
-  def list_insert(e, l) do
-  #   :
-  end
-
-end
--- bench/lib/tree.ex
+++
@@ -1,9 +0,0 @@
-
-defmodule Tree do
-
-  ef tree_new() do ... end
-
-  def tree_insert(e, l) do
- #    :f
-  end
-end
