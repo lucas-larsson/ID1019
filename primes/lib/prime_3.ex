e6dc7aa6a3d5c8ac758e2c438826b76405fa3c81
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-11 02:46:55 +0100

    added the third method()

--- primes/lib/prime_3.ex
+++ primes/lib/prime_3.ex
@@ -1,8 +1,30 @@
 defmodule Prime_3  do
 
-  def prime(n) do
-
-  end
-
+ def prime(n) do
+   list = Enum.to_list(2..n)
+   prime(list,[])
+ end
+ def prime(list, primes) do
+   case list do
+     [] -> Enum.reverse(primes)
+     [h|t] ->
+       bool = checkPrime(list, primes)
+       primes = insert(bool, h, primes)
+       prime(t, primes)
+   end
+ end
+ def checkPrime(_, []) do true end
+ def checkPrime([h|t], [h2|t2]) do
+   cond do  (rem(h,h2) == 0) ->
+       false
+       true -> checkPrime([h|t], t2)
+   end
+ end
+ def insert(bool, x, primes) do
+   case bool do
+     true -> primes ++ [x]
+     false -> primes
+   end
+ end
 
 end
