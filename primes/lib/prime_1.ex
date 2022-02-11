8af517936d27a9620c4a7d9da81dc7b028a6f5d1
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-11 13:26:19 +0100

    renamed method

--- primes/lib/prime_1.ex
+++ primes/lib/prime_1.ex
@@ -2,13 +2,13 @@ defmodule Prime_1  do
 
   def prime(n) do
     [h|t] = Enum.to_list(2..n)
-    [h | remove(h,t)]
+    [h | filter(h,t)]
 
   end
 
-  def remove(_, []) do [] end
-  def remove(x,[h|t]) do
-    [h|remove(h, Enum.filter(t, fn p -> rem(p, x) != 0 end))]
+  def filter(_, []) do [] end
+  def filter(x,[h|t]) do
+    [h|filter(h, Enum.filter(t, fn p -> rem(p, x) != 0 end))]
     # Enum.reject([1, 2, 3], fn x -> rem(x, 2) == 0 end)
       #[1, 3]
   end
