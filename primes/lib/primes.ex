4e3106604881dd0b4fc695815171f94a5bff7c8d
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-11 02:47:52 +0100

    moded the output prints

--- primes/lib/primes.ex
+++ primes/lib/primes.ex
@@ -17,16 +17,19 @@ defmodule Primes do
   end
 
   def bench(n) do
-    # IO.write(:wakadudle11111)
-    # IO.inspect(Prime_1.prime(n))
-    # IO.inspect(elem(:timer.tc(fn -> Prime_1.prime(n) end),0))
-     IO.inspect(:timer.tc(fn -> Prime_1.prime(n) end))
+    IO.write('Runtime for 1 ')
+    IO.inspect(elem(:timer.tc(fn -> Prime_1.prime(n) end),0))
+    #  IO.inspect(:timer.tc(fn -> Prime_1.prime(n) end))
 
-    IO.write(:wakad222222)
-    # IO.inspect(elem(:timer.tc(fn -> Prime_2.prime(n) end),0))
-    IO.inspect(:timer.tc(fn -> Prime_2.prime(n) end))
-    # IO.inspect(elem(:timer.tc(fn -> Prime_3.prime(n) end),0))
+    IO.write('Runtime for 2 ')
+    IO.inspect(elem(:timer.tc(fn -> Prime_2.prime(n) end),0))
+    # IO.inspect(:timer.tc(fn -> Prime_2.prime(n) end))
+
+    IO.write('Runtime for 3 ')
+    IO.inspect(elem(:timer.tc(fn -> Prime_3.prime(n) end),0))
+    # IO.inspect(:timer.tc(fn -> Prime_3.prime(n) end))
+
+
     :ok
   end
 end
-
