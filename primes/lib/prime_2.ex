8930874c70624fd0b235f33ef2ea2f107697c080
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-11 15:20:14 +0100

    removed redundent code

--- primes/lib/prime_2.ex
+++ primes/lib/prime_2.ex
@@ -29,13 +29,5 @@ defmodule Prime_2  do
       false -> primes
     end
   end
-
-  def remove(_, []) do [] end
-  def remove(x,[h|t], []) do
-    [h|remove(h, Enum.filter(t, fn p -> rem(p, x) != 0 end))]
-    # Enum.reject([1, 2, 3], fn x -> rem(x, 2) == 0 end)
-      #[1, 3]
-
-  end
-
+  
 end
