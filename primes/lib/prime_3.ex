e2308c78786feab5df9eeb547a4ae828206fa315
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-11 13:26:58 +0100

    removed duplicate method

--- primes/lib/prime_3.ex
+++ primes/lib/prime_3.ex
@@ -8,21 +8,14 @@ defmodule Prime_3  do
    case list do
      [] -> Enum.reverse(primes)
      [h|t] ->
-       bool = checkPrime(list, primes)
+       bool = Prime_2.checkPrime(list, primes)
        primes = insert(bool, h, primes)
        prime(t, primes)
    end
  end
- def checkPrime(_, []) do true end
- def checkPrime([h|t], [h2|t2]) do
-   cond do  (rem(h,h2) == 0) ->
-       false
-       true -> checkPrime([h|t], t2)
-   end
- end
  def insert(bool, x, primes) do
    case bool do
-     true -> primes ++ [x]
+     true ->  [x|primes] #improper 
      false -> primes
    end
  end
