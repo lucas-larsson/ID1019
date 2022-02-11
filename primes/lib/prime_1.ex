f0a61e4090c7e60f0bd8ac5dc42bf1521d5e0115
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-11 15:17:51 +0100

    removed comment

--- primes/lib/prime_1.ex
+++ primes/lib/prime_1.ex
@@ -9,8 +9,7 @@ defmodule Prime_1  do
   def filter(_, []) do [] end
   def filter(x,[h|t]) do
     [h|filter(h, Enum.filter(t, fn p -> rem(p, x) != 0 end))]
-    # Enum.reject([1, 2, 3], fn x -> rem(x, 2) == 0 end)
-      #[1, 3]
+    
   end
 
 end
