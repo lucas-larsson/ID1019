5586749689ff835e7c7f5d79fbacd4fa75940b9c
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-21 21:32:13 +0100

    added memo module

---
+++ lumberjack/lib/memo.ex
@@ -0,0 +1,10 @@
+defmodule Memo do
+
+  def new() do %{} end
+
+  def add(mem, key, val) do Map.put(mem, :binary.list_to_bin(key), val) end
+
+  def lookup(mem, key) do Map.get(mem, :binary.list_to_bin(key)) end
+
+
+end
