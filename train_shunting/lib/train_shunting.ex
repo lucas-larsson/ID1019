de5213da3f134981a862f509467b6b33c6e57951
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-24 20:31:58 +0100

    added Train functions

--- train_shunting/lib/train_shunting.ex
+++ train_shunting/lib/train_shunting.ex
@@ -1,4 +1,4 @@
-defmodule TrainShunting do
+defmodule Train do
   @moduledoc """
   Documentation for `TrainShunting`.
   """
@@ -6,7 +6,7 @@ defmodule TrainShunting do
   @doc """
   Hello world.
 
-  ## Examples
+  Examples
 
       iex> TrainShunting.hello()
       :world
@@ -15,4 +15,43 @@ defmodule TrainShunting do
   def hello do
     :world
   end
+  
+  def split(list, atom) do
+    case Train.member(list, atom) do
+        false -> :sus
+        true -> {Train.take(list, Train.position(list, atom)-1), Train.drop(list, Train.position(list, atom))}
+    end
+  end
+
+
+ # +1 for length of y
+ def find(xs, ys) do
+     case ys do
+         [] -> []
+         [y|t] ->
+             {hs, ts} = split(xs, y)
+             step = Moves.single({:one, length(ts)+1}, {xs, [], []})
+             step = Moves.single({:two, length(hs)}, step)
+             step = Moves.single({:one, -(length(ts)+1)}, step)
+             {[_|t2], [], []} = Moves.single({:two, -(length(hs))}, step)
+
+             [{:one, length(ts)+1}, {:two, length(hs)}, {:one, -(length(ts)+1)}, {:two, -(length(hs))}| find(t2, t)]
+     end
+ end
+
+ def few([u|k], [u|t]) do few(k, t) end
+ def few(xs, ys) do
+     case ys do
+         [] -> []
+         [y|t] ->
+             {hs, ts} = split(xs, y)
+             step = Moves.single({:one, length(ts)+1}, {xs, [], []})
+             step = Moves.single({:two, length(hs)}, step)
+             step = Moves.single({:one, -(length(ts)+1)}, step)
+             {[_|t2], [], []} = Moves.single({:two, -(length(hs))}, step)
+
+             [{:one, length(ts)+1}, {:two, length(hs)}, {:one, -(length(ts)+1)}, {:two, -(length(hs))}| few(t2, t)]
+     end
+ end
+
 end
