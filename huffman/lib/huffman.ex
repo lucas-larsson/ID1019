1e91f786bb4edd6fcddcc3739da17c3f2590ddc2
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-27 14:07:42 +0100

    added Huffman assignment

---
+++ huffman/lib/huffman.ex
@@ -0,0 +1,18 @@
+defmodule Huffman do
+  @moduledoc """
+  Documentation for `Huffman`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Huffman.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
