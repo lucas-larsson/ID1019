261b3a155ef697ae4be873ed795c67334148a28f
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-27 17:27:05 +0100

    added frequency copunter

---
+++ huffman/lib/frequency.ex
@@ -0,0 +1,20 @@
+defmodule Freq do
+
+  # Compute the frequencies of all the characters in the
+  # sample text and return a list of tuples {char, freq}.
+  def freq(sample), do: freq(sample, [])
+
+  def freq([], freq), do: freq
+  def freq([char | rest], freq) do
+    freq(rest, update(char, freq))
+  end
+
+  def update(char, []), do: [{char, 1}]
+  def update(char, [{char, n} | freq]) do
+    [{char, n + 1} | freq]
+  end
+  def update(char, [elem | freq]) do
+    [elem | update(char, freq)]
+  end
+
+end
