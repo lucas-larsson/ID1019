0878c3b14ce30bb177735af8ef67a0552fa91362
Lucas Larsson <lucas.a.larsson@gmail.com>
2022-02-28 18:51:29 +0100

    added Philosophers assignment

---
+++ philosophers/lib/philosophers.ex
@@ -0,0 +1,18 @@
+defmodule Philosophers do
+  @moduledoc """
+  Documentation for `Philosophers`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Philosophers.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
